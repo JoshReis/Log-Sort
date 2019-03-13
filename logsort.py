717a992805b846e93b7a4912807ca61e7ea91f30
Joshua Reisbord <joshuareisbord@gmail.com>
2019-03-12 21:56:44 -0400

    Each line is now checked and and written to the file individually. Any file size can be used.
    Added count too how many times the phrase was found
    Reports how long the process took.

--- logsort.py
+++ logsort.py
@@ -1,53 +1,42 @@
 """
 This utility allows you to create a new .txt file from a log file containing
 ONLY the lines which contain the key phrase defined by the user.
-
 The data from the log file is read, split, and stored in a list, later to be
 passed to a linear search function which searches for the key phrase in ALL
 lines stored in the list and if the condition is met, the line is written back
 to the log file in the initial format which is was read as.
-
 NOTES:
 - Files created and read from same directory as logsort.py
-
 - Word split is the character in your log which separates each word. For
   example if my line is "Hello World" a space would be my word split.
-
 Author: J. Reisbord
 Date: Feb 2, 2019.
 """
+import sys
+import time
 
-
-def file_to_list(file, split):
+def file_to_list(file, split, phrase):
     """
     :param file:  File which will be turned into a list of lists
     :param split: What defines a new block of data in each line
-    :return: Log file as a list
+    :param phrase: Phrase to look for in each line.
+    :return: New log file.
     """
-    converted_data = []
+    count = 0
+    outfile = open('sorted_log.txt', 'w+')  # Creates a new file
+    time_start = time.time()
 
     with open(file) as data:
-
         for line in data:  # For each line in 'file'
             data_list = [str(do.strip()) for do in line.split(split)]
             # Split elements at ',' and strip trailing new line
-            converted_data.append(data_list)  # Appended data to converted_data
+            if phrase in data_list:  # Checks for key phrase
+                count += 1
+                sys.stdout.write("\r" + "Found the phrase: '" + phrase + "' " + str(count) + " times.")
+                sys.stdout.flush()
+                outfile.write(" ".join(data_list) + '\n')  # Writes to file.
 
-        return converted_data  # Returns converted data.
-
-
-def write_by_phrase(converted_log, phrase, split):
-    """
-    :param converted_log: List of lines from log file.
-    :param phrase: The phrase which must be contained for the line to be written
-    :return: Nothing.
-    """
-    outfile = open('sorted_log.txt', 'w+')  # Creates a new file
-
-    for element in converted_log:  # Loops for all lines in converted_log
-        if phrase in element:  # Checks for key phrase
-            print(split.join(element))
-            outfile.write(" ".join(element) + '\n')  # Writes too file.
+    print("\nTook ", round(time.time() - time_start, 2), "to complete.")
 
 
 def main():
@@ -63,8 +52,7 @@ def main():
             log = input(str('Log file: '))
             word = input(str('Search phrase: '))
             split = input(str('Word split: '))
-            listed_log = (file_to_list(log, split))  # Turns log too list of lists
-            write_by_phrase(listed_log, word, split)  # Writes to file.
+            file_to_list(log, split, word)  # Turns log too list of lists
             print('\nAll data has been outputted to sorted_log.txt')
 
         except FileNotFoundError:
